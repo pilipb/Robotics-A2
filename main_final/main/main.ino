f73da41a475549b702af99bea517972b7f99a3a1
pilipb <xq19351@bristol.ac.uk>
2024-03-13 14:44:29 +0000

    runs ok on my robot
    note that over steers

--- main_final/main/main.ino
+++ main_final/main/main.ino
@@ -45,7 +45,7 @@ float left_motor_demand;          // Demand for the left motor
 float MOTOR_SPEED = 75;   // Deafult forwards motor speed
 float MAX_MOTOR_STEP_SIZE = 5; // Maximum step size for ramping the motor speed
 float TURN_SPEED = 20;    // Default turning speed
-float TURN_ON_SPOT_SPEED = 10; // Default turning speed on the spot
+float TURN_ON_SPOT_SPEED = 20; // Default turning speed on the spot
 
 // Definition of states
 #define STATE_INITIAL 0
@@ -53,95 +53,96 @@ float TURN_ON_SPOT_SPEED = 10; // Default turning speed on the spot
 #define TURN 2
 #define STATE_FINISHED 3
 #define STATE_DEBUG 4
+#define STATE_FINAL 5
 int state; // Variable to store the current state
 
 // square stuff:
-float SQUARE_SIZE = 200; // Size of the square in mm
+float SQUARE_SIZE = 300; // Size of the square in mm
 int side_count = 0; // Count of the number of sides of the square
 int num_squares = 1; // Number of squares to complete
 
 void drive_along_angle(float angle) {
 
-    heading_error = theta_i - angle; // Calculate the heading error
+  heading_error = theta_i - angle; // Calculate the heading error
 
-    // Set the motor demand accordingly
-    right_motor_demand = MOTOR_SPEED + heading_control;
-    left_motor_demand = MOTOR_SPEED - heading_control;
+  // Set the motor demand accordingly
+  right_motor_demand = MOTOR_SPEED + heading_control;
+  left_motor_demand = MOTOR_SPEED - heading_control;
 }
 
 void rotate_on_spot(float angle) {
 
-    heading_error = theta_i - angle; // Calculate the heading error
+  heading_error = theta_i - angle; // Calculate the heading error
 
-    // Set the motor demand accordingly
-    right_motor_demand =  heading_error * (-TURN_ON_SPOT_SPEED);
-    left_motor_demand = heading_error * TURN_ON_SPOT_SPEED;
+  // Set the motor demand accordingly
+  right_motor_demand =  heading_error * (-TURN_ON_SPOT_SPEED);
+  left_motor_demand = heading_error * TURN_ON_SPOT_SPEED;
 }
 
 void rotate_on_spot_relative(float angle) {
 
-    float target_angle = theta_i + angle;
+  float target_angle = theta_i + angle;
 
-    heading_error = theta_i - target_angle; // Calculate the heading error
+  heading_error = theta_i - target_angle; // Calculate the heading error
 
-    // Set the motor demand accordingly
-    right_motor_demand =  TURN_SPEED * (-heading_error);
-    left_motor_demand = TURN_SPEED * heading_error;
+  // Set the motor demand accordingly
+  right_motor_demand =  TURN_SPEED * (-heading_error);
+  left_motor_demand = TURN_SPEED * heading_error;
 }
 
 void rotate_around_wheel(float angle) {
 
-    heading_error = theta_i - angle; // Calculate the heading error
-
-    // Set the motor demand accordingly
-    if (heading_error < 0) {
-        right_motor_demand = 2*TURN_SPEED * (-heading_error);
-        left_motor_demand = 0;
-    }
-    else {
-      right_motor_demand = 0;
-      left_motor_demand = 2*TURN_SPEED * heading_error;
-    }
-}
-
-void join_line() {
-  
-  if (!line_detected) {
-    drive_along_angle(0); // Drive forwards
-  }
-
-  if (!line_detected && line[1] && line[2] && line[3]) {
-    last_line_detected = millis();
-    line_detected = true;
-  }
-
-  if (line_detected) {
-    drive_along_angle(0);
-    if (millis() - last_line_detected >= REJOIN_LINE_TIME) {
-      rotate_on_spot(PI);
-    }
-  }
-}
-
-void follow_line() {
-
-  // Calculate the heading error
-  heading_error = line_sensors.normalise_sensor_readings(sensor_readings);
-
-  if (line[0] && !line[4]) {
-    heading_error = 0.8;
-  }
-  else if (!line[0] && line[4]) {
-    heading_error = -0.8;
-  }
+  heading_error = theta_i - angle; // Calculate the heading error
 
   // Set the motor demand accordingly
-  left_motor_demand = MOTOR_SPEED + heading_control;
-  right_motor_demand = MOTOR_SPEED - heading_control;
-
-  // Save the global angle of the line
-  last_line_angle = theta_i;
+  if (heading_error < 0) {
+    right_motor_demand = 2 * TURN_SPEED * (-heading_error);
+    left_motor_demand = 0;
+  }
+  else {
+    right_motor_demand = 0;
+    left_motor_demand = 2 * TURN_SPEED * heading_error;
+  }
 }
+
+//void join_line() {
+//
+//  if (!line_detected) {
+//    drive_along_angle(0); // Drive forwards
+//  }
+//
+//  if (!line_detected && line[1] && line[2] && line[3]) {
+//    last_line_detected = millis();
+//    line_detected = true;
+//  }
+//
+//  if (line_detected) {
+//    drive_along_angle(0);
+//    if (millis() - last_line_detected >= REJOIN_LINE_TIME) {
+//      rotate_on_spot(PI);
+//    }
+//  }
+//}
+
+//void follow_line() {
+//
+//  // Calculate the heading error
+//  heading_error = line_sensors.normalise_sensor_readings(sensor_readings);
+//
+//  if (line[0] && !line[4]) {
+//    heading_error = 0.8;
+//  }
+//  else if (!line[0] && line[4]) {
+//    heading_error = -0.8;
+//  }
+//
+//  // Set the motor demand accordingly
+//  left_motor_demand = MOTOR_SPEED + heading_control;
+//  right_motor_demand = MOTOR_SPEED - heading_control;
+//
+//  // Save the global angle of the line
+//  last_line_angle = theta_i;
+//}
 
 void rejoin_line() {
 
@@ -150,41 +151,41 @@ void rejoin_line() {
 }
 
 void integral_reset() {
-    // Reset the integral sum of the PID controllers
-    heading_PID.integral_sum = 0;
-    r_motor_PID.integral_sum = 0;
-    l_motor_PID.integral_sum = 0;
+  // Reset the integral sum of the PID controllers
+  heading_PID.integral_sum = 0;
+  r_motor_PID.integral_sum = 0;
+  l_motor_PID.integral_sum = 0;
 }
+//
+//void intersection() {
+//  if (intersection_count > 3) {
+//    turn_right = false;
+//  }
+//
+//  if (millis() - intersection_ts < 10) {
+//    drive_along_angle(last_line_angle);
+//  }
+//
+//  else {
+//    if (turn_right) {
+//      left_motor_demand = TURN_SPEED;
+//      right_motor_demand = -TURN_SPEED;
+//    }
+//    else {
+//      left_motor_demand = -TURN_SPEED;
+//      right_motor_demand = TURN_SPEED;
+//    }
+//  }
+//}
 
-void intersection() {
-  if (intersection_count > 3) {
-    turn_right = false;
-  }
-
-  if (millis() - intersection_ts < 10) {
-    drive_along_angle(last_line_angle);
-  }
-
-  else {
-    if (turn_right) {
-      left_motor_demand = TURN_SPEED;
-      right_motor_demand = -TURN_SPEED;
-    }
-    else {
-      left_motor_demand = -TURN_SPEED;
-      right_motor_demand = TURN_SPEED;
-    }
-  }
-}
-
-void crossroads() {
-    
-  if (millis() - intersection_ts < 500) {
-    drive_along_angle(last_line_angle);
-  } else {
-    rotate_on_spot(-PI/2);
-  }
-}
+//void crossroads() {
+//
+//  if (millis() - intersection_ts < 500) {
+//    drive_along_angle(last_line_angle);
+//  } else {
+//    rotate_on_spot(-PI/2);
+//  }
+//}
 
 
 float angle_to_home() {
@@ -198,7 +199,7 @@ void face_home() {
 }
 
 void return_home() {
-    drive_along_angle(angle_to_home());
+  drive_along_angle(angle_to_home());
 }
 
 
@@ -220,7 +221,7 @@ void setup() {
 
   // Set the initial timestamp
   line_sensor_ts = millis();
-  PID_ts = millis();  
+  PID_ts = millis();
   kinematic_ts = millis();
   motor_ts = millis();
   ramp_ts = millis();
@@ -238,7 +239,7 @@ void loop() {
 
   // LINE SENSOR UPDATE
   if ((current_ts - line_sensor_ts) >= LINE_SENSOR_UPDATE) {
-    
+
     line_sensor_ts = millis(); // record the current timestamp
 
     line_sensors.read_all_sensors_digital(sensor_readings); // Read the sensor values
@@ -248,11 +249,11 @@ void loop() {
 
   // KINEMATIC UPDATE
   if ((current_ts - kinematic_ts) >= KINEMATIC_UPDATE) {
-    
+
     kinematic_ts = current_ts; // Update the timestamp
 
     kinematics.update(); // Update the kinematic model
-    }
+  }
 
   // PID UPDATE
   if ((current_ts - PID_ts) >= PID_UPDATE) {
@@ -269,7 +270,7 @@ void loop() {
     left_control = l_motor_PID.update(left_motor_demand, rotation_speed_l_smoothed);
   }
 
-    // MOTOR UPDATE
+  // MOTOR UPDATE
   if ((current_ts - motor_ts) >= MOTOR_UPDATE) {
 
     motor_ts = millis(); // Update the timestamp
@@ -277,49 +278,51 @@ void loop() {
     motors.set_motor(left_control, right_control, MAX_MOTOR_STEP_SIZE); // Set the motor speeds
   }
 
-  float side_theta = side_count * PI/2;
+  float side_theta = side_count * PI / 2;
 
   // DECIDE STATE:
   if (state == STATE_INITIAL && (millis() - timer >= 1500)) {
     state = STATE_DRIVE_FORWARD;
-  } 
+  }
+
+  else if (state == TURN && side_count >= (( 4 * num_squares) - 1)) {
+    state = STATE_FINAL;
+  }
 
   else if (state == STATE_DRIVE_FORWARD && side_count % 4 == 0 && (x_i >= SQUARE_SIZE)) {
-    buzzer.beep(1000,200);
+    //    buzzer.beep(1000, 200);
     state = TURN;
   }
 
   else if (state == STATE_DRIVE_FORWARD && side_count % 4 == 1 && (y_i >= SQUARE_SIZE)) {
-    buzzer.beep(1000,200);
+    //    buzzer.beep(1000, 200);
     state = TURN;
   }
 
   else if (state == STATE_DRIVE_FORWARD && side_count % 4 == 2 && (x_i <= 0)) {
-    buzzer.beep(1000,200);
+    //    buzzer.beep(1000, 200);
     state = TURN;
   }
 
   else if (state == STATE_DRIVE_FORWARD && side_count % 4 == 3 && (y_i <= 0)) {
-    buzzer.beep(1000,200);
+    //    buzzer.beep(1000, 200);
     state = TURN;
   }
 
-  else if (state == TURN && theta_i <= ((side_count+1)*PI/2)) {
-    buzzer.beep(1000,200);
+  else if (state == TURN && theta_i <= ((side_count + 1)*PI / 2)) {
+    //    buzzer.beep(1000, 200);
     state = STATE_DRIVE_FORWARD;
     side_count++;
-    }
-
-  else if (side_count > 4*num_squares -1) {
-    state = STATE_INITIAL
-  }  
+  }
 
   // STATE MACHINE:
-  if (state == STATE_INITIAL) {
-      // Do nothing
+  if (state == STATE_INITIAL || state == STATE_FINAL) {
+    // Do nothing
+    right_motor_demand =  0;
+    left_motor_demand = 0;
 
   } else if (state == STATE_DRIVE_FORWARD) {
-    float angle = side_count * PI/2;
+    float angle = side_count * PI / 2;
     drive_along_angle(angle);
 
   } else if (state == TURN) {
@@ -327,37 +330,37 @@ void loop() {
     right_motor_demand =  TURN_ON_SPOT_SPEED;
     left_motor_demand = -TURN_ON_SPOT_SPEED;
 
-  } else if (state == STATE_FOLLOW_LINE) {
-
-    follow_line();
-
-  } else if (state == STATE_REJOIN_LINE) {
-
-    rejoin_line();
-
-  } else if (state == STATE_INTERSECTION) {
-      
-    intersection();
-
-  } else if (state == STATE_CROSSROADS) {
-        
-      crossroads();
-
-  } else if (state == STATE_FACE_HOME) {
-      
-      face_home();
-
-  } else if (state == STATE_RTH) {
-      
-    return_home();
-
-  } else if (state == STATE_FINISHED) {
-
-    // Stop the motors
-    motors.set_motor(0, 0, MAX_MOTOR_STEP_SIZE);
-    if (millis() - timer <= 1500) {
-      buzzer.beep(1000,10);
-    }
+    //  } else if (state == STATE_FOLLOW_LINE) {
+    //
+    //    follow_line();
+    //
+    //  } else if (state == STATE_REJOIN_LINE) {
+    //
+    //    rejoin_line();
+    //
+    //  } else if (state == STATE_INTERSECTION) {
+    //
+    //    intersection();
+    //
+    //  } else if (state == STATE_CROSSROADS) {
+    //
+    //    crossroads();
+    //
+    //  } else if (state == STATE_FACE_HOME) {
+    //
+    //    face_home();
+    //
+    //  } else if (state == STATE_RTH) {
+    //
+    //    return_home();
+    //
+    //  } else if (state == STATE_FINISHED) {
+    //
+    //    // Stop the motors
+    //    motors.set_motor(0, 0, MAX_MOTOR_STEP_SIZE);
+    //    if (millis() - timer <= 1500) {
+    //      buzzer.beep(1000, 10);
+    //    }
 
   } else if (state == STATE_DEBUG) {
 
@@ -365,7 +368,7 @@ void loop() {
 
     // rotate_on_spot(PI);
     // drive_along_angle(0);
-// MOTOR CONTROL:
+    // MOTOR CONTROL:
     // Serial.print("left:");
     // Serial.print(left_control);
     // Serial.print(" ");
@@ -384,13 +387,12 @@ void loop() {
     Serial.print("rotation_speed_l:");
     Serial.println(rotation_speed_l_smoothed);
 
-// KINEMATICS:
-  //   Serial.print(x_i);
-  //   Serial.print(" ");
-  //   Serial.print(y_i);
-  //   Serial.print(" ");
-  //   Serial.println(theta_i);
- }
+    // KINEMATICS:
+    //   Serial.print(x_i);
+    //   Serial.print(" ");
+    //   Serial.print(y_i);
+    //   Serial.print(" ");
+    //   Serial.println(theta_i);
+  }
 
 };
-
